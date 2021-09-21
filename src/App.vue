50b983f2f6cfb4036bd9ec50670d02a100339407
Lino Toran Jenner <lino@toranjenner.de>
2021-09-21 16:38:35 +0200

    silas v1

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,41 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="box">
+      <h1 class="title is-2">Current DJ</h1>
+        <div class="columns is-vcentered">
+          <div class="column">
+          <div class="image is-256x256">
+            <img src="https://bulma.io/images/placeholders/256x256.png" />
+            <h1 class="title is-3 pt-2">Barleton</h1>
+          </div>
+          </div>
+            <div class="column">
+              <h1 class="title giantcolor">5:00</h1>
+            </div>
+            <div class="column" >
+              <button class="button is-success is-large" style="width: 100%; height: 100%"> + 0:30 </button>
+            </div>
+          
+
+          <br />
+          <h1>{{ timer }}</h1>
+          <br />
+        </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import "@/myStyle.sass";
+import "font-awesome/css/font-awesome.min.css";
 
 export default {
   name: "App",
-  components: {
-    HelloWorld,
+  data() {
+    return {};
   },
+  mounted() {},
+  computed: {},
 };
 </script>
 
@@ -22,7 +45,12 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #000000;
+}
+
+
+.giantcolor {
+  font-size: 120px;
+  color: coral;
 }
 </style>
